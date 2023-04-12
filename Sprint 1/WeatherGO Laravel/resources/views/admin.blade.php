a177dc39752c0249003f775ec3eb2815770e64f6
jaaak24 <fahreza107@gmail.com>
2023-04-12 13:17:57 +0700

    sprint 1

---
+++ Sprint 1/WeatherGO Laravel/resources/views/admin.blade.php
@@ -0,0 +1,160 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+<title>WeatherGO</title>
+<link rel="stylesheet" type="text/css" href="{{asset('styles/bootstrap.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('styles/style.css')}}">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="{{asset('fonts/css/fontawesome-all.min.css')}}">
+<link rel="manifest" href="{{asset('_manifest.json')}}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{asset('app/icons/icon-192x192.png')}}">
+<link rel="shortcut icon" href="{{asset('images/logo.png')}}" type="image/x-icon">
+</head>
+
+<body class="theme-light" data-highlight="highlight-red">
+
+<div id="preloader"><div class="spinner-border color-highlight" role="status"></div></div>
+
+<div id="page">
+    
+    <div class="header header-auto-show header-fixed header-logo-center">
+        <a href="{{route('home')}}" class="header-title">WeatherGO</a>
+        <a href="#" data-menu="menu-main" class="header-icon header-icon-1"><i class="fas fa-bars"></i></a>
+        <a href="#" data-toggle-theme class="header-icon header-icon-4 show-on-theme-dark"><i class="fas fa-sun"></i></a>
+        <a href="#" data-toggle-theme class="header-icon header-icon-4 show-on-theme-light"><i class="fas fa-moon"></i></a>
+        <a href="#" data-menu="menu-share" class="header-icon header-icon-3"><i class="fas fa-share-alt"></i></a>
+    </div>
+
+    <div id="footer-bar" class="footer-bar-6">
+        <a href="https://www.google.com/maps"><i class="fa fa-layer-group"></i><span>Maps</span></a>
+        <a href="https://timeline.google.com/maps/timeline?hl=en&authuser=0&pli=1&rapt=AEjHL4MPnuDuKxJK938FR1ZYdnZxZA4wm2ELbhVFEASy8solDsMq6ukbJOhUBuSsQB_GcFFY8ZGMdMjJnJxxYm29ngFk1c5VCw&pb=!1m2!1m1!1s"><i class="fa fa-file"></i><span>History</span></a>
+        <a href="{{route('home')}}" class="circle-nav active-nav"><i class="fa fa-home"></i><span>Welcome</span></a>
+        <a href="{{route('galery')}}"><i class="fa fa-image"></i><span>Galery</span></a>
+        <a href="#" data-menu="menu-main"><i class="fa fa-bars"></i><span>Menu</span></a>
+    </div>
+
+    <div class="page-title page-title-fixed">
+        <h1>WeatherGO</h1>
+        <a href="#" class="page-title-icon shadow-xl bg-theme color-theme" data-menu="menu-share"><i class="fa fa-share-alt"></i></a>
+        <a href="#" class="page-title-icon shadow-xl bg-theme color-theme show-on-theme-light" data-toggle-theme><i class="fa fa-moon"></i></a>
+        <a href="#" class="page-title-icon shadow-xl bg-theme color-theme show-on-theme-dark" data-toggle-theme><i class="fa fa-lightbulb color-yellow-dark"></i></a>
+        <a href="#" class="page-title-icon shadow-xl bg-theme color-theme" data-menu="menu-main"><i class="fa fa-bars"></i></a>
+    </div>
+    <div class="page-title-clear"></div>
+
+    <div id="menu-main" class="menu menu-box-left rounded-0" data-menu-width="280" data-menu-active="nav-welcome" data-menu-load="{{route('main')}}"></div>
+    <div id="menu-share" class="menu menu-box-bottom rounded-m"  data-menu-load="menu-share.blade.php" data-menu-height="370"> </div>
+    <div id="menu-colors" class="menu menu-box-bottom rounded-m" data-menu-load="{{route('colors')}}" data-menu-height="480"></div>
+
+
+    <h1>Feedback</h1>
+    <table class="table dataTable no-footer" id="dataTable" width="100%" cellspacing="0" role="grid" aria-describedby="dataTable_info" style="width: 100%;">
+        <thead>
+    <thead>
+        <tr role="row"><th style="width: 19.5208px;" class="sorting sorting_asc" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-sort="ascending" aria-label="
+                No
+            : activate to sort column descending">
+                ID
+            </th><th style="width: 149.667px;" class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                Nama Lengkap
+            : activate to sort column ascending">
+                Nama 
+            </th><th style="width: 245.5px;" class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                Desa
+            : activate to sort column ascending">
+                Email
+            </th><th class="sorting" tabindex="0" aria-controls="dataTable" rowspan="1" colspan="1" aria-label="
+                Kategori
+            : activate to sort column ascending" style="width: 100.188px;">
+                Message</th>
+        </thead>
+        <tbody> 
+            @foreach ($feedback as $item)
+            <tr role="row" class="odd">
+                <td class="sorting_1">{{$item->id}}</td>
+                <td>{{$user->where('id', $item->user_id)->first()->firstname}}</td>
+                <td>{{$item->email}}</td>
+                <td>{{$item->message}}</td>
+            </tr>
+            @endforeach
+        </tbody>                                                                                              
+        </table>
+    
+
+    <div data-menu-load="{{route('footer')}}"></div>
+
+    <!-- All Menus, Action Sheets, Modals, Notifications, Toasts, Snackbars get Placed outside the <div class="page-content"> -->
+
+    <!-- Menu Share -->
+    <div id="menu-share" class="menu menu-box-bottom menu-box-detached">
+        <div class="menu-title mt-n1"><h1>Share the Love</h1><p class="color-highlight">Just Tap the Social Icon. We'll add the Link</p><a href="#" class="close-menu"><i class="fa fa-times"></i></a></div>
+        <div class="content mb-0">
+            <div class="divider mb-0"></div>
+            <div class="list-group list-custom-small list-icon-0">
+                <a href="auto_generated" class="shareToFacebook external-link">
+                    <i class="font-18 fab fa-facebook-square color-facebook"></i>
+                    <span class="font-13">Facebook</span>
+                    <i class="fa fa-angle-right"></i>
+                </a>
+                <a href="auto_generated" class="shareToTwitter external-link">
+                    <i class="font-18 fab fa-twitter-square color-twitter"></i>
+                    <span class="font-13">Twitter</span>
+                    <i class="fa fa-angle-right"></i>
+                </a>
+                <a href="auto_generated" class="shareToLinkedIn external-link">
+                    <i class="font-18 fab fa-linkedin color-linkedin"></i>
+                    <span class="font-13">LinkedIn</span>
+                    <i class="fa fa-angle-right"></i>
+                </a>
+                <a href="auto_generated" class="shareToWhatsApp external-link">
+                    <i class="font-18 fab fa-whatsapp-square color-whatsapp"></i>
+                    <span class="font-13">WhatsApp</span>
+                    <i class="fa fa-angle-right"></i>
+                </a>
+                <a href="auto_generated" class="shareToMail external-link border-0">
+                    <i class="font-18 fa fa-envelope-square color-mail"></i>
+                    <span class="font-13">Email</span>
+                    <i class="fa fa-angle-right"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Be sure this is on your main visiting page, for example, the index.html page-->
+    <!-- Install Prompt for Android -->
+    <div id="menu-install-pwa-android" class="menu menu-box-bottom rounded-m">
+        <img class="mx-auto mt-4 rounded-m" src="app/icons/icon-128x128.png" alt="img" width="90">
+        <h4 class="text-center mt-4 mb-2">WeatherGO on your Home Screen</h4>
+        <p class="text-center boxed-text-xl">
+            Install WeatherGO on your home screen, and access it just like a regular app. It really is that simple!
+        </p>
+        <div class="boxed-text-l">
+            <a href="#" class="pwa-install mx-auto btn btn-m font-600 bg-highlight">Add to Home Screen</a>
+            <a href="#" class="pwa-dismiss close-menu btn-full mt-3 pt-2 text-center text-uppercase font-600 color-red-light font-12 pb-4 mb-3">Maybe later</a>
+        </div>
+    </div>
+
+    <!-- Install instructions for iOS -->
+    <div id="menu-install-pwa-ios" class="menu menu-box-bottom rounded-m">
+        <div class="boxed-text-xl top-25">
+            <img class="mx-auto mt-4 rounded-m" src="app/icons/icon-128x128.png" alt="img" width="90">
+            <h4 class="text-center mt-4 mb-2">WeatherGO on your Home Screen</h4>
+            <p class="text-center ms-3 me-3">
+                Install WeatherGO on your home screen, and access it just like a regular app. Open your Safari menu and tap "Add to Home Screen".
+            </p>
+            <a href="#" class="pwa-dismiss close-menu btn-full mt-3 text-center text-uppercase font-700 color-red-light opacity-90 font-110 pb-5">Maybe later</a>
+        </div>
+    </div>
+
+</div>
+
+<script type="text/javascript" src="{{asset('scripts/bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('scripts/custom.js')}}"></script>
+
+</body>
+</html>
---
+++ Sprint 1/WeatherGO Laravel/resources/views/page-user-edit.blade.php
@@ -0,0 +1,187 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+<title>WeatherGO</title>
+<link rel="stylesheet" type="text/css" href="{{asset('styles/bootstrap.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('styles/style.css')}}">
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="{{asset('fonts/css/fontawesome-all.min.css')}}">
+<link rel="manifest" href="{{asset('_manifest.json')}}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{asset('app/icons/icon-192x192.png')}}">
+<link rel="shortcut icon" href="{{asset('images/logo.png')}}" type="image/x-icon">
+</head>
+    
+<body class="theme-light">
+    
+<div id="preloader"><div class="spinner-border color-highlight" role="status"></div></div>
+    
+<div id="page">
+    
+    <div class="header header-fixed header-logo-center header-auto-show">
+        <a href="{{route('profile')}}" class="header-title">Edit Profile </a>
+        <a href="#" data-back-button class="header-icon header-icon-1"><i class="fas fa-chevron-left"></i></a>
+        <a href="#" data-menu="menu-main" class="header-icon header-icon-4"><i class="fas fa-bars"></i></a>
+        <a href="#" data-toggle-theme class="header-icon header-icon-3 show-on-theme-dark"><i class="fas fa-sun"></i></a>
+        <a href="#" data-toggle-theme class="header-icon header-icon-3 show-on-theme-light"><i class="fas fa-moon"></i></a>
+    </div>
+
+    <div id="footer-bar" class="footer-bar-6">
+        <a href="index-components.html"><i class="fa fa-layer-group"></i><span>Features</span></a>
+        <a href="index-pages.html" class="active-nav"><i class="fa fa-file"></i><span>Pages</span></a>
+        <a href="{{route('home')}}" class="circle-nav"><i class="fa fa-home"></i><span>Welcome</span></a>
+        <a href="index-projects.html"><i class="fa fa-camera"></i><span>Projects</span></a>
+        <a href="#" data-menu="menu-main"><i class="fa fa-bars"></i><span>Menu</span></a>
+    </div>
+    
+    <div class="page-title page-title-fixed">
+        <h1>Edit Profile</h1>
+        <a href="#" class="page-title-icon shadow-xl bg-theme color-theme" data-menu="menu-share"><i class="fa fa-share-alt"></i></a>
+        <a href="#" class="page-title-icon shadow-xl bg-theme color-theme show-on-theme-light" data-toggle-theme><i class="fa fa-moon"></i></a>
+        <a href="#" class="page-title-icon shadow-xl bg-theme color-theme show-on-theme-dark" data-toggle-theme><i class="fa fa-lightbulb color-yellow-dark"></i></a>
+        <a href="#" class="page-title-icon shadow-xl bg-theme color-theme" data-menu="menu-main"><i class="fa fa-bars"></i></a>
+    </div>
+    <div class="page-title-clear"></div>
+        
+    <div class="page-content">
+             
+        <div class="card card-style bg-28" data-card-height="450">
+            <div class="card-top">
+                <a href="{{route('uploadImage')}}" class="btn btn-s border-white color-white float-end mt-2 me-2 rounded-sm"><i class="fa fa-camera pe-2"></i>Upload </a>
+                <a href="{{asset('profile_image/'.$user->iamge)}}" class="glightbox"><img src="{{asset('profile_image/'.$user->image)}}" class="menu-img img-fluid" alt=""></a>
+            </div>
+            <div class="card-bottom ms-3 me-3">
+                <h1 class="font-40 line-height-xl color-white">{{$user->firstname}}<br>{{$user->lastname}}</h1>
+                <p class="color-white opacity-60"><i class="fa fa-map-marker me-2"></i>{{$user->location}}</p>
+                <p class="color-white opacity-80 font-15">
+                    {{$user->about}}
+                </p>
+            </div>
+            <div class="card-overlay bg-gradient"></div>
+        </div>
+
+        <div class="card card-style">
+            <div class="content">
+                <form action="{{route('storeProfile')}}" method="post">
+                    @csrf
+                    <p class="mb-n1 color-highlight font-600 font-12">Edit your Account</p>
+                    <h4>Basic Information</h4>
+                    <p>
+                        Public information that shows on top of your card in your profile page. This is just a dummy page.
+                    </p>
+                        
+                    <div class="mt-5 mb-3">
+                        
+                        <div class="input-style has-borders no-icon input-style-always-active mb-4">
+                            <input type="text" class="form-control" id="firstname" name="firstname" placeholder="First Name">
+                            <label for="firstname" class="color-highlight">First Name</label>
+                            <em>(required)</em>
+                        </div>
+                        <div class="input-style has-borders no-icon input-style-always-active mb-4">
+                            <input type="text" class="form-control" id="lastname" name="lastname" placeholder="Last Name">
+                            <label for="lastname" class="color-highlight">Last Name</label>
+                            <em>(required)</em>
+                        </div>
+                        <div class="input-style has-borders no-icon input-style-always-active mb-4">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="name@domain.com">
+                            <label for="email" class="color-highlight">Email Address</label>
+                            <em>(required)</em>
+                        </div>
+                        <div class="input-style has-borders no-icon input-style-always-active mb-4">
+                            <input type="text" class="form-control" id="location" name="location" placeholder="San Francisco, California">
+                            <label for="location" class="color-highlight">Location</label>
+                            <em>(required)</em>
+                        </div>
+                        <div class="input-style has-borders no-icon input-style-always-active mb-4">
+                            <textarea id="about" name="about" placeholder="Enter your message"></textarea>
+                            <label for="about" class="color-highlight">Enter your Message</label>
+                        </div>                    
+                    </div>
+
+                    <center>
+                        <button  class="btn btn-full btn-m gradient-highlight rounded-s font-13 font-600 mt-4">Save Basic Information</button>
+                    </center>
+                </form>
+            </div>
+        </div>
+            
+        <div class="card card-style">
+            <div class="content">
+                
+                <p class="mb-n1 color-highlight font-600 font-12">Edit your General</p>
+                <h4>Privacy Settings</h4>
+                <p>
+                    Private information that doesn't show on top of your card in your profile page. This is just a dummy page.
+                </p>
+
+                <div class="d-flex mb-3">
+                    <div class="pt-1">
+                        <h5 data-data-trigger-switch="toggle-id-1" class="font-600 font-14">Private Profile</h5>
+                    </div>
+                    <div class="ms-auto me-3 pe-2">
+                        <div class="custom-control ios-switch small-switch">
+                            <input type="checkbox" class="ios-input" id="toggle-id-1">
+                            <label class="custom-control-label" for="toggle-id-1"></label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex mb-3">
+                    <div class="pt-1">
+                        <h5 data-data-trigger-switch="toggle-id-2" class="font-600 font-14">Share Location</h5>
+                    </div>
+                    <div class="ms-auto me-3 pe-2">
+                        <div class="custom-control ios-switch small-switch">
+                            <input type="checkbox" class="ios-input" id="toggle-id-2">
+                            <label class="custom-control-label" for="toggle-id-2"></label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex mb-3">
+                    <div class="pt-1">
+                        <h5 data-data-trigger-switch="toggle-id-3" class="font-600 font-14">Subscribe to  Mails</h5>
+                    </div>
+                    <div class="ms-auto me-3 pe-2">
+                        <div class="custom-control ios-switch small-switch">
+                            <input type="checkbox" class="ios-input" id="toggle-id-3" checked>
+                            <label class="custom-control-label" for="toggle-id-3"></label>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex mb-3">
+                    <div class="pt-1">
+                        <h5 data-data-trigger-switch="toggle-id-4" class="font-600 font-14">Hide Email Address</h5>
+                    </div>
+                    <div class="ms-auto me-3 pe-2">
+                        <div class="custom-control ios-switch small-switch">
+                            <input type="checkbox" class="ios-input" id="toggle-id-4" checked>
+                            <label class="custom-control-label" for="toggle-id-4"></label>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+        <div data-menu-load="{{route('footer')}}"></div>
+    </div>
+    <!-- Page content ends here-->
+    
+    <!-- Main Menu--> 
+    <div id="menu-main" class="menu menu-box-left rounded-0" data-menu-load="{{route('main')}}" data-menu-width="280" data-menu-active="nav-pages"></div>
+    
+    <!-- Share Menu-->
+    <div id="menu-share" class="menu menu-box-bottom rounded-m" data-menu-load="menu-share.html" data-menu-height="370"></div>  
+    
+    <!-- Colors Menu-->
+    <div id="menu-colors" class="menu menu-box-bottom rounded-m" data-menu-load="{{route('colors')}}" data-menu-height="480"></div> 
+     
+    
+</div>
+
+<script type="text/javascript" src="{{asset('scripts/bootstrap.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('scripts/custom.js')}}"></script>
+
+</body>
