7aeaa177a218cdbd2cf32dd04f2f5327898f041f
Pradika <bvnpradikaa@student.telkomuniversity.ac.id>
2023-06-07 00:19:51 +0700

    update backlog #25

--- Sprint 3/WeatherGO Laravel/resources/views/admin.blade.php
+++ Sprint 3/WeatherGO Laravel/resources/views/admin.blade.php
@@ -61,7 +61,7 @@
                 </div>
                 <div class="pe-3">
                     <h5 class="color-white">{{$userCount}}</h5>
-                    <h6 class="color-white opacity-60">Use</h6>
+                    <h6 class="color-white opacity-60">User</h6>
                 </div>
                 <div class="pe-3">
                     <h5 class="color-white">{{$feedbackCount}}</h5>
