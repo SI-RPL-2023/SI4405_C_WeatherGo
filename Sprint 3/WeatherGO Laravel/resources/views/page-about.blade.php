fcc56f82499fec9a176c78f695ca60aa1d25c51b
anisawahyup <anisawahyupratiwi01@gmail.com>
2023-06-07 06:24:45 +0700

    memperbarui bagian about us

--- Sprint 3/WeatherGO Laravel/resources/views/page-about.blade.php
+++ Sprint 3/WeatherGO Laravel/resources/views/page-about.blade.php
@@ -22,7 +22,7 @@
 <div id="page">
     
     <div class="header header-fixed header-logo-center header-auto-show">
-        <a href="index.html" class="header-title">About U </a>
+        <a href="index.html" class="header-title">About Us </a>
         <a href="#" data-back-button class="header-icon header-icon-1"><i class="fas fa-chevron-left"></i></a>
         <a href="#" data-menu="menu-main" class="header-icon header-icon-4"><i class="fas fa-bars"></i></a>
         <a href="#" data-toggle-theme class="header-icon header-icon-3 show-on-theme-dark"><i class="fas fa-sun"></i></a>
