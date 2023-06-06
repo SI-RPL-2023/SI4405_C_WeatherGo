b10cdac417e949c9bf4c26790829f8a547f5a1ad
MuhammadDaffaAuliaRahman <muhdffaulrhmn@student.telkomuniversity.ac.id>
2023-06-06 21:55:16 +0700

    progress codingan product backlog FOOTER (done)

--- Sprint 3/WeatherGO Laravel/resources/views/menu-footer.blade.php
+++ Sprint 3/WeatherGO Laravel/resources/views/menu-footer.blade.php
@@ -13,8 +13,8 @@
     </div>
     <div class="divider mb-3"></div>
     <div class="row text-center mb-3 pl-3 pr-3">
-        <a class="font-11 col-4" href="#">Privacy Policys</a>
-        <a class="font-11 col-4" href="#">Terms of ServiceS</a>
+        <a class="font-11 col-4" href="#">Privacy Policy</a>
+        <a class="font-11 col-4" href="#">Terms of Service</a>
         <a class="font-11 col-4" href="#">Contact Support</a>
     </div>
     
