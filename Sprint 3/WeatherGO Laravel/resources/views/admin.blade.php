1ad8e40d42736c62abdc6158fb74e7106b6df813
Pradika <bvnpradikaa@student.telkomuniversity.ac.id>
2023-06-07 00:21:16 +0700

    update backlog #25

--- Sprint 3/WeatherGO Laravel/resources/views/admin.blade.php
+++ Sprint 3/WeatherGO Laravel/resources/views/admin.blade.php
@@ -133,7 +133,7 @@
             <div class="col-6">
                 <div class="card mx-0 card-style" data-card-height="260">
                     <div class="content">
-                        <h5 class="font-14 opacity-50">Running Website<a href="#" class="float-end color-highlight"><i class="fa fa-arrow-right"></i></a></h5>
+                        <h5 class="font-14 opacity-50">Running<a href="#" class="float-end color-highlight"><i class="fa fa-arrow-right"></i></a></h5>
                         <div class="divider mb-3"></div>
                     </div>
                     <div class="card-center text-center pb-4">
