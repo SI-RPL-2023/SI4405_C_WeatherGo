ec4b216acf2accd8f9eca29b2685aef5f3063cfe
muhafif <afifaidn@gmail.cm>
2023-06-06 22:18:45 +0700

    memperbaiki footer admin -7

--- Sprint 3/WeatherGO Laravel/resources/views/menu-footer.blade.php
+++ Sprint 3/WeatherGO Laravel/resources/views/menu-footer.blade.php
@@ -9,7 +9,7 @@
         <a href="#" class="icon icon-xs rounded-sm shadow-l mr-1 bg-twitter"><i class="fab fa-twitter"></i></a>
         <a href="#" class="icon icon-xs rounded-sm shadow-l mr-1 bg-phone"><i class="fa fa-phone"></i></a>
         <a href="#" data-menu="menu-share" class="icon icon-xs rounded-sm mr-1 shadow-l bg-red-dark"><i class="fa fa-share-alt"></i></a>
-        <a href="#" class="back-to-top icon icon-xs rounded-sm shadow-l bg-highlight color-white"><i class="fa fa-arrow-up"></i></a>
+        <a href="#" class="back-to-top icon icon-xs rounded-sm shadow-l bg-highlight color-white"><i class="fa fa-arro-up"></i></a>
     </div>
     <div class="divider mb-3"></div>
     <div class="row text-center mb-3 pl-3 pr-3">
