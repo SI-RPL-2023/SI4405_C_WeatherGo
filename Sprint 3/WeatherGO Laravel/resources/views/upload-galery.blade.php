bbad5e15015ae233c8fd81a6ed31b67b42a73199
muhafif <afifaidn@gmail.cm>
2023-06-06 22:09:06 +0700

    memperbaiki fitur upload gambar ke galeri lagi

--- Sprint 3/WeatherGO Laravel/resources/views/upload-galery.blade.php
+++ Sprint 3/WeatherGO Laravel/resources/views/upload-galery.blade.php
@@ -23,7 +23,7 @@
     <div class="header header-fixed header-logo-center header-auto-show">
         <a href="{{route('uploadImage')}}" class="header-title">File Upload</a>
         <a href="#" data-back-button class="header-icon header-icon-1"><i class="fas fa-chevron-left"></i></a>
-        <a href="#" data-menu="menu-main" class="header-icon header-icon-4"><i cla="fas fa-bars"></i></a>
+        <a href="#" data-menu="menu-main" class="header-icon header-icon-4"><i class="fas fa-bars"></i></a>
         <a href="#" data-toggle-theme class="header-icon header-icon-3 show-on-theme-dark"><i class="fas fa-sun"></i></a>
         <a href="#" data-toggle-theme class="header-icon header-icon-3 show-on-theme-light"><i class="fas fa-moon"></i></a>
     </div>
