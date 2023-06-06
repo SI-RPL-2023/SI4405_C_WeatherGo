441f1f708ec4eb2c2a6ce4a7db338e83f8629018
MuhammadDaffaAuliaRahman <muhdffaulrhmn@student.telkomuniversity.ac.id>
2023-06-06 21:31:50 +0700

    progress backlog discuss

--- Sprint 3/WeatherGO Laravel/resources/views/discuss.blade.php
+++ Sprint 3/WeatherGO Laravel/resources/views/discuss.blade.php
@@ -51,7 +51,7 @@
         <div class="card card-style">
             <div class="content">
                 <p>
-                    Comments, reactions, and share
+                    Comments, reactions, and shar
                 </p>
             </div>
         </div>
